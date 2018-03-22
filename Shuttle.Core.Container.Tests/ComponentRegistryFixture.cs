074b890f28bd10ad01915ede17a5914e1f28eee4
Zavarkó Gábor <zavarkog@gmail.com>
2018-03-22 16:08:29 +0100

    Small improvements + Fix tabs + remove unnecessary space in nuspec

--- Shuttle.Core.Container.Tests/ComponentRegistryFixture.cs
+++ Shuttle.Core.Container.Tests/ComponentRegistryFixture.cs
@@ -4,210 +4,210 @@ using NUnit.Framework;
 
 namespace Shuttle.Core.Container.Tests
 {
-	[TestFixture]
-	public class ComponentRegistryExtensionsFixture
-	{
-		[Test]
-		public void Should_be_able_to_register_as_default_singleton()
-		{
-			var mock = new Mock<IComponentRegistry>();
+    [TestFixture]
+    public class ComponentRegistryExtensionsFixture
+    {
+        [Test]
+        public void Should_be_able_to_register_as_default_singleton()
+        {
+            var mock = new Mock<IComponentRegistry>();
 
-			mock.Object.Register<ISomeDependency, SomeDependency>();
-			mock.Object.Register<ISomeDependency, SomeDependency>();
+            mock.Object.Register<ISomeDependency, SomeDependency>();
+            mock.Object.Register<ISomeDependency, SomeDependency>();
 
-			mock.Verify(m => m.IsRegistered(It.IsAny<Type>()), Times.Never);
-			mock.Verify(m => m.Register(typeof(ISomeDependency), typeof(SomeDependency), Lifestyle.Singleton), Times.Exactly(2));
-		}
+            mock.Verify(m => m.IsRegistered(It.IsAny<Type>()), Times.Never);
+            mock.Verify(m => m.Register(typeof(ISomeDependency), typeof(SomeDependency), Lifestyle.Singleton), Times.Exactly(2));
+        }
 
-		[Test]
-		public void Should_be_able_to_register_as_singleton()
-		{
-			var mock = new Mock<IComponentRegistry>();
+        [Test]
+        public void Should_be_able_to_register_as_singleton()
+        {
+            var mock = new Mock<IComponentRegistry>();
 
-			mock.Object.Register<ISomeDependency, SomeDependency>(Lifestyle.Singleton);
-			mock.Object.Register<ISomeDependency, SomeDependency>(Lifestyle.Singleton);
+            mock.Object.Register<ISomeDependency, SomeDependency>(Lifestyle.Singleton);
+            mock.Object.Register<ISomeDependency, SomeDependency>(Lifestyle.Singleton);
 
-			mock.Verify(m => m.IsRegistered(It.IsAny<Type>()), Times.Never);
-			mock.Verify(m => m.Register(typeof(ISomeDependency), typeof(SomeDependency), Lifestyle.Singleton), Times.Exactly(2));
-		}
+            mock.Verify(m => m.IsRegistered(It.IsAny<Type>()), Times.Never);
+            mock.Verify(m => m.Register(typeof(ISomeDependency), typeof(SomeDependency), Lifestyle.Singleton), Times.Exactly(2));
+        }
 
-		[Test]
-		public void Should_be_able_to_register_as_transient()
-		{
-			var mock = new Mock<IComponentRegistry>();
+        [Test]
+        public void Should_be_able_to_register_as_transient()
+        {
+            var mock = new Mock<IComponentRegistry>();
 
-			mock.Object.Register<ISomeDependency, SomeDependency>(Lifestyle.Transient);
-			mock.Object.Register<ISomeDependency, SomeDependency>(Lifestyle.Transient);
+            mock.Object.Register<ISomeDependency, SomeDependency>(Lifestyle.Transient);
+            mock.Object.Register<ISomeDependency, SomeDependency>(Lifestyle.Transient);
 
-			mock.Verify(m => m.IsRegistered(It.IsAny<Type>()), Times.Never);
-			mock.Verify(m => m.Register(typeof(ISomeDependency), typeof(SomeDependency), Lifestyle.Transient), Times.Exactly(2));
-		}
+            mock.Verify(m => m.IsRegistered(It.IsAny<Type>()), Times.Never);
+            mock.Verify(m => m.Register(typeof(ISomeDependency), typeof(SomeDependency), Lifestyle.Transient), Times.Exactly(2));
+        }
 
-		[Test]
-		public void Should_be_able_to_register_implementation_as_default_singleton()
-		{
-			var mock = new Mock<IComponentRegistry>();
+        [Test]
+        public void Should_be_able_to_register_implementation_as_default_singleton()
+        {
+            var mock = new Mock<IComponentRegistry>();
 
-			mock.Object.Register<SomeDependency>();
-			mock.Object.Register<SomeDependency>();
+            mock.Object.Register<SomeDependency>();
+            mock.Object.Register<SomeDependency>();
 
-			mock.Verify(m => m.IsRegistered(It.IsAny<Type>()), Times.Never);
-			mock.Verify(m => m.Register(typeof(SomeDependency), typeof(SomeDependency), Lifestyle.Singleton), Times.Exactly(2));
-		}
+            mock.Verify(m => m.IsRegistered(It.IsAny<Type>()), Times.Never);
+            mock.Verify(m => m.Register(typeof(SomeDependency), typeof(SomeDependency), Lifestyle.Singleton), Times.Exactly(2));
+        }
 
-		[Test]
-		public void Should_be_able_to_register_implementation_as_singleton()
-		{
-			var mock = new Mock<IComponentRegistry>();
+        [Test]
+        public void Should_be_able_to_register_implementation_as_singleton()
+        {
+            var mock = new Mock<IComponentRegistry>();
 
-			mock.Object.Register<SomeDependency>(Lifestyle.Singleton);
-			mock.Object.Register<SomeDependency>(Lifestyle.Singleton);
+            mock.Object.Register<SomeDependency>(Lifestyle.Singleton);
+            mock.Object.Register<SomeDependency>(Lifestyle.Singleton);
 
-			mock.Verify(m => m.IsRegistered(It.IsAny<Type>()), Times.Never);
-			mock.Verify(m => m.Register(typeof(SomeDependency), typeof(SomeDependency), Lifestyle.Singleton), Times.Exactly(2));
-		}
+            mock.Verify(m => m.IsRegistered(It.IsAny<Type>()), Times.Never);
+            mock.Verify(m => m.Register(typeof(SomeDependency), typeof(SomeDependency), Lifestyle.Singleton), Times.Exactly(2));
+        }
 
-		[Test]
-		public void Should_be_able_to_register_implementation_as_transient()
-		{
-			var mock = new Mock<IComponentRegistry>();
+        [Test]
+        public void Should_be_able_to_register_implementation_as_transient()
+        {
+            var mock = new Mock<IComponentRegistry>();
 
-			mock.Object.Register<SomeDependency>(Lifestyle.Transient);
-			mock.Object.Register<SomeDependency>(Lifestyle.Transient);
+            mock.Object.Register<SomeDependency>(Lifestyle.Transient);
+            mock.Object.Register<SomeDependency>(Lifestyle.Transient);
 
-			mock.Verify(m => m.IsRegistered(It.IsAny<Type>()), Times.Never);
-			mock.Verify(m => m.Register(typeof(SomeDependency), typeof(SomeDependency), Lifestyle.Transient), Times.Exactly(2));
-		}
+            mock.Verify(m => m.IsRegistered(It.IsAny<Type>()), Times.Never);
+            mock.Verify(m => m.Register(typeof(SomeDependency), typeof(SomeDependency), Lifestyle.Transient), Times.Exactly(2));
+        }
 
-		[Test]
-		public void Should_be_able_to_register_instance()
-		{
-			var mock = new Mock<IComponentRegistry>();
+        [Test]
+        public void Should_be_able_to_register_instance()
+        {
+            var mock = new Mock<IComponentRegistry>();
 
-			mock.Object.RegisterInstance<ISomeDependency>(new SomeDependency());
-			mock.Object.RegisterInstance<ISomeDependency>(new SomeDependency());
+            mock.Object.RegisterInstance<ISomeDependency>(new SomeDependency());
+            mock.Object.RegisterInstance<ISomeDependency>(new SomeDependency());
 
-			mock.Verify(m => m.IsRegistered(It.IsAny<Type>()), Times.Never);
-			mock.Verify(m => m.RegisterInstance(typeof(ISomeDependency), It.IsAny<SomeDependency>()), Times.Exactly(2));
-		}
+            mock.Verify(m => m.IsRegistered(It.IsAny<Type>()), Times.Never);
+            mock.Verify(m => m.RegisterInstance(typeof(ISomeDependency), It.IsAny<SomeDependency>()), Times.Exactly(2));
+        }
 
-		[Test]
-		public void Should_be_able_to_attempt_register_as_default_singleton()
-		{
-			var mock = new Mock<IComponentRegistry>();
+        [Test]
+        public void Should_be_able_to_attempt_register_as_default_singleton()
+        {
+            var mock = new Mock<IComponentRegistry>();
 
-			mock.Setup(m => m.IsRegistered(typeof(ISomeDependency))).Returns(false);
+            mock.Setup(m => m.IsRegistered(typeof(ISomeDependency))).Returns(false);
 
-			mock.Object.AttemptRegister<ISomeDependency, SomeDependency>();
+            mock.Object.AttemptRegister<ISomeDependency, SomeDependency>();
 
-			mock.Setup(m => m.IsRegistered(typeof(ISomeDependency))).Returns(true);
+            mock.Setup(m => m.IsRegistered(typeof(ISomeDependency))).Returns(true);
 
-			mock.Object.AttemptRegister<ISomeDependency, SomeDependency>();
+            mock.Object.AttemptRegister<ISomeDependency, SomeDependency>();
 
-			mock.Verify(m => m.IsRegistered(It.IsAny<Type>()), Times.Exactly(2));
-			mock.Verify(m => m.Register(typeof(ISomeDependency), typeof(SomeDependency), Lifestyle.Singleton), Times.Once);
-		}
+            mock.Verify(m => m.IsRegistered(It.IsAny<Type>()), Times.Exactly(2));
+            mock.Verify(m => m.Register(typeof(ISomeDependency), typeof(SomeDependency), Lifestyle.Singleton), Times.Once);
+        }
 
-		[Test]
-		public void Should_be_able_to_attempt_register_as_singleton()
-		{
-			var mock = new Mock<IComponentRegistry>();
+        [Test]
+        public void Should_be_able_to_attempt_register_as_singleton()
+        {
+            var mock = new Mock<IComponentRegistry>();
 
-			mock.Setup(m => m.IsRegistered(typeof(ISomeDependency))).Returns(false);
+            mock.Setup(m => m.IsRegistered(typeof(ISomeDependency))).Returns(false);
 
-			mock.Object.AttemptRegister<ISomeDependency, SomeDependency>(Lifestyle.Singleton);
+            mock.Object.AttemptRegister<ISomeDependency, SomeDependency>(Lifestyle.Singleton);
 
-			mock.Setup(m => m.IsRegistered(typeof(ISomeDependency))).Returns(true);
+            mock.Setup(m => m.IsRegistered(typeof(ISomeDependency))).Returns(true);
 
-			mock.Object.AttemptRegister<ISomeDependency, SomeDependency>(Lifestyle.Singleton);
+            mock.Object.AttemptRegister<ISomeDependency, SomeDependency>(Lifestyle.Singleton);
 
-			mock.Verify(m => m.IsRegistered(It.IsAny<Type>()), Times.Exactly(2));
-			mock.Verify(m => m.Register(typeof(ISomeDependency), typeof(SomeDependency), Lifestyle.Singleton), Times.Once);
-		}
+            mock.Verify(m => m.IsRegistered(It.IsAny<Type>()), Times.Exactly(2));
+            mock.Verify(m => m.Register(typeof(ISomeDependency), typeof(SomeDependency), Lifestyle.Singleton), Times.Once);
+        }
 
-		[Test]
-		public void Should_be_able_to_attempt_register_as_transient()
-		{
-			var mock = new Mock<IComponentRegistry>();
+        [Test]
+        public void Should_be_able_to_attempt_register_as_transient()
+        {
+            var mock = new Mock<IComponentRegistry>();
 
-			mock.Setup(m => m.IsRegistered(typeof(ISomeDependency))).Returns(false);
+            mock.Setup(m => m.IsRegistered(typeof(ISomeDependency))).Returns(false);
 
-			mock.Object.AttemptRegister<ISomeDependency, SomeDependency>(Lifestyle.Transient);
+            mock.Object.AttemptRegister<ISomeDependency, SomeDependency>(Lifestyle.Transient);
 
-			mock.Setup(m => m.IsRegistered(typeof(ISomeDependency))).Returns(true);
+            mock.Setup(m => m.IsRegistered(typeof(ISomeDependency))).Returns(true);
 
-			mock.Object.AttemptRegister<ISomeDependency, SomeDependency>(Lifestyle.Transient);
+            mock.Object.AttemptRegister<ISomeDependency, SomeDependency>(Lifestyle.Transient);
 
-			mock.Verify(m => m.IsRegistered(It.IsAny<Type>()), Times.Exactly(2));
-			mock.Verify(m => m.Register(typeof(ISomeDependency), typeof(SomeDependency), Lifestyle.Transient), Times.Once);
-		}
+            mock.Verify(m => m.IsRegistered(It.IsAny<Type>()), Times.Exactly(2));
+            mock.Verify(m => m.Register(typeof(ISomeDependency), typeof(SomeDependency), Lifestyle.Transient), Times.Once);
+        }
 
-		[Test]
-		public void Should_be_able_to_attempt_register_implementation_as_default_singleton()
-		{
-			var mock = new Mock<IComponentRegistry>();
+        [Test]
+        public void Should_be_able_to_attempt_register_implementation_as_default_singleton()
+        {
+            var mock = new Mock<IComponentRegistry>();
 
-			mock.Setup(m => m.IsRegistered(typeof(SomeDependency))).Returns(false);
+            mock.Setup(m => m.IsRegistered(typeof(SomeDependency))).Returns(false);
 
-			mock.Object.AttemptRegister<SomeDependency>();
+            mock.Object.AttemptRegister<SomeDependency>();
 
-			mock.Setup(m => m.IsRegistered(typeof(SomeDependency))).Returns(true);
+            mock.Setup(m => m.IsRegistered(typeof(SomeDependency))).Returns(true);
 
-			mock.Object.AttemptRegister<SomeDependency>();
+            mock.Object.AttemptRegister<SomeDependency>();
 
-			mock.Verify(m => m.IsRegistered(It.IsAny<Type>()), Times.Exactly(2));
-			mock.Verify(m => m.Register(typeof(SomeDependency), typeof(SomeDependency), Lifestyle.Singleton), Times.Once);
-		}
+            mock.Verify(m => m.IsRegistered(It.IsAny<Type>()), Times.Exactly(2));
+            mock.Verify(m => m.Register(typeof(SomeDependency), typeof(SomeDependency), Lifestyle.Singleton), Times.Once);
+        }
 
-		[Test]
-		public void Should_be_able_to_attempt_register_implementation_as_singleton()
-		{
-			var mock = new Mock<IComponentRegistry>();
+        [Test]
+        public void Should_be_able_to_attempt_register_implementation_as_singleton()
+        {
+            var mock = new Mock<IComponentRegistry>();
 
-			mock.Setup(m => m.IsRegistered(typeof(SomeDependency))).Returns(false);
+            mock.Setup(m => m.IsRegistered(typeof(SomeDependency))).Returns(false);
 
-			mock.Object.AttemptRegister<SomeDependency>(Lifestyle.Singleton);
+            mock.Object.AttemptRegister<SomeDependency>(Lifestyle.Singleton);
 
-			mock.Setup(m => m.IsRegistered(typeof(SomeDependency))).Returns(true);
+            mock.Setup(m => m.IsRegistered(typeof(SomeDependency))).Returns(true);
 
-			mock.Object.AttemptRegister<SomeDependency>(Lifestyle.Singleton);
+            mock.Object.AttemptRegister<SomeDependency>(Lifestyle.Singleton);
 
-			mock.Verify(m => m.IsRegistered(It.IsAny<Type>()), Times.Exactly(2));
-			mock.Verify(m => m.Register(typeof(SomeDependency), typeof(SomeDependency), Lifestyle.Singleton), Times.Once);
-		}
+            mock.Verify(m => m.IsRegistered(It.IsAny<Type>()), Times.Exactly(2));
+            mock.Verify(m => m.Register(typeof(SomeDependency), typeof(SomeDependency), Lifestyle.Singleton), Times.Once);
+        }
 
-		[Test]
-		public void Should_be_able_to_attempt_register_implementation_as_transient()
-		{
-			var mock = new Mock<IComponentRegistry>();
+        [Test]
+        public void Should_be_able_to_attempt_register_implementation_as_transient()
+        {
+            var mock = new Mock<IComponentRegistry>();
 
-			mock.Setup(m => m.IsRegistered(typeof(SomeDependency))).Returns(false);
+            mock.Setup(m => m.IsRegistered(typeof(SomeDependency))).Returns(false);
 
-			mock.Object.AttemptRegister<SomeDependency>(Lifestyle.Transient);
+            mock.Object.AttemptRegister<SomeDependency>(Lifestyle.Transient);
 
-			mock.Setup(m => m.IsRegistered(typeof(SomeDependency))).Returns(true);
+            mock.Setup(m => m.IsRegistered(typeof(SomeDependency))).Returns(true);
 
-			mock.Object.AttemptRegister<SomeDependency>(Lifestyle.Transient);
+            mock.Object.AttemptRegister<SomeDependency>(Lifestyle.Transient);
 
-			mock.Verify(m => m.IsRegistered(It.IsAny<Type>()), Times.Exactly(2));
-			mock.Verify(m => m.Register(typeof(SomeDependency), typeof(SomeDependency), Lifestyle.Transient), Times.Once);
-		}
+            mock.Verify(m => m.IsRegistered(It.IsAny<Type>()), Times.Exactly(2));
+            mock.Verify(m => m.Register(typeof(SomeDependency), typeof(SomeDependency), Lifestyle.Transient), Times.Once);
+        }
 
-		[Test]
-		public void Should_be_able_to_attempt_register_instance()
-		{
-			var mock = new Mock<IComponentRegistry>();
+        [Test]
+        public void Should_be_able_to_attempt_register_instance()
+        {
+            var mock = new Mock<IComponentRegistry>();
 
-			mock.Setup(m => m.IsRegistered(typeof(ISomeDependency))).Returns(false);
+            mock.Setup(m => m.IsRegistered(typeof(ISomeDependency))).Returns(false);
 
-			mock.Object.AttemptRegisterInstance<ISomeDependency>(new SomeDependency());
+            mock.Object.AttemptRegisterInstance<ISomeDependency>(new SomeDependency());
 
-			mock.Setup(m => m.IsRegistered(typeof(ISomeDependency))).Returns(true);
+            mock.Setup(m => m.IsRegistered(typeof(ISomeDependency))).Returns(true);
 
-			mock.Object.AttemptRegisterInstance<ISomeDependency>(new SomeDependency());
+            mock.Object.AttemptRegisterInstance<ISomeDependency>(new SomeDependency());
 
-			mock.Verify(m => m.IsRegistered(It.IsAny<Type>()), Times.Exactly(2));
-			mock.Verify(m => m.RegisterInstance(typeof(ISomeDependency), It.IsAny<SomeDependency>()), Times.Once);
-		}
-	}
+            mock.Verify(m => m.IsRegistered(It.IsAny<Type>()), Times.Exactly(2));
+            mock.Verify(m => m.RegisterInstance(typeof(ISomeDependency), It.IsAny<SomeDependency>()), Times.Once);
+        }
+    }
 }
